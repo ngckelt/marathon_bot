678aa24fdc316e2ebfdde2bfddc3ff09ff604eeb
nick <kamarinskiv@mail.ru>
2021-10-06 20:24:41 +0700

    Сделал кнопку начала марафона рабочей

--- handlers/users/marathon_members/__init__.py
+++ handlers/users/marathon_members/__init__.py
@@ -1,3 +1,4 @@
+from .start_marathon import dp
 from .registration import dp
 from .update_data import dp
 # from .timestamps import dp
---
+++ handlers/users/marathon_members/start_marathon/__init__.py
@@ -0,0 +1,5 @@
+from .start_marathon import dp
+
+
+__all__ = ["dp"]
+
