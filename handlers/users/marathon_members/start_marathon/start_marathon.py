6c093a1a106f15bb2765cc08264b14efe546b93d
nick <kamarinskiv@mail.ru>
2021-10-06 20:27:15 +0700

    Сделал кнопку начала марафона рабочей

--- handlers/users/marathon_members/start_marathon/start_marathon.py
+++ handlers/users/marathon_members/start_marathon/start_marathon.py
@@ -7,14 +7,17 @@ from utils.db_api.db import MarathonMembersModel
 @dp.message_handler(text="Начать марафон")
 async def start_marathon(message: types.Message):
     marathon_member = await MarathonMembersModel.get_marathon_member(message.from_user.id)
-    if marathon_member is None:
-        await MarathonMembersModel.update_marathon_member(message.from_user.id, on_marathon=True)
-        await message.answer(
-            text="Марафон начался. С завтрашнего дня тебе нужно будет присылать отчеты",
-            reply_markup=main_markup
-        )
+    if marathon_member is not None:
+        if not marathon_member.on_marathon:
+            await MarathonMembersModel.update_marathon_member(message.from_user.id, on_marathon=True)
+            await message.answer(
+                text="Марафон начался. С завтрашнего дня тебе нужно будет присылать отчеты",
+                reply_markup=main_markup
+            )
+        else:
+            await message.answer("Вы уже участвуете в марафоне")
     else:
-        await message.answer("Вы уже участвуете в марафоне")
+        await message.answer("Для начала зарегистрируйтесь в боте")
 
 
 
